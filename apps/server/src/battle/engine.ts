d38e1b7782f5da96915a078e307b9041fb40c036
contato-anselmojr <contato-anselmojr@github.com>
2025-10-16 18:59:11 -0300

    auto: backup autom├ítico 2025-10-16_18-59-11

--- apps/server/src/battle/engine.ts
+++ apps/server/src/battle/engine.ts
@@ -1,18 +1,18 @@
-import { Skill, EnergyCost } from "./types";
+﻿import { Skill, EnergyCost } from "./types";
 
 export function calculateSkillCost(skill: Skill): EnergyCost {
   // Sistema atualizado baseado em nomes completos (RED, BLUE, WHITE, GREEN)
   const cost: EnergyCost = { RED: 0, BLUE: 0, WHITE: 0, GREEN: 0 };
+
   if (Array.isArray(skill.cost)) {
     for (const c of skill.cost) {
-      if (cost[c] -ne $null) {
-        cost[c]++
+      if (cost[c as keyof EnergyCost] !== undefined) {
+        cost[c as keyof EnergyCost]++;
       }
     }
-  } elseif ($skill.cost -is [hashtable]) {
-    foreach ($key in $skill.cost.Keys) {
-      $cost[$key] = $skill.cost[$key]
-    }
+  } else if (typeof skill.cost === "object" && skill.cost !== null) {
+    Object.assign(cost, skill.cost);
   }
+
   return cost;
 }
